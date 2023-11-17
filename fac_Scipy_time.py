0c14bc7b0b1fc68fb6a3c77677321778e29eb050
Dibakar2020 <paldibakar7718@gmail.com>
2023-11-17 22:48:15 +0530

    time calculation to exicute factorial calculation Using Python, but with the factorial function from SciPy

---
+++ fac_Scipy_time.py
@@ -0,0 +1,13 @@
+from scipy.special import factorial
+import timeit
+from scipy.special import factorial
+import timeit
+# Function to calculate factorial using SciPy
+def calculate_factorial(n):
+    return factorial(n)
+if __name__ == "__main__":
+    n = 5 # Example: Calculate factorial of 5
+    elapsed_time = timeit.timeit(lambda: calculate_factorial(n), number=100000)
+    result = calculate_factorial(n)
+    print(f"Factorial of {n} is {result}")
+    print(f"Time taken: {elapsed_time:.10f} seconds")
